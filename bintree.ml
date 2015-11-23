24ccd40fa2d5b0c08d3e223e5ec10e1ef06712a7
Antoine Girard <antoine.girard@eisti.fr>
2015-11-23 15:36:45 +0100

    Add ABR predicat + binary tree search

--- bintree.ml
+++ bintree.ml
@@ -6,7 +6,10 @@
 
 include Printf
 
+(* Type Binary Tree *)
 type 'a tree = Nul | Node of 'a * 'a tree * 'a tree;;
+
+(* Classical Function *)
 
 let createTree v ll lr = Node (v,ll,lr);;
 
@@ -47,4 +50,82 @@ let rec size a =
   match a with
   | Nul -> 0
   | Node (_,ll,lr) -> 1 + (size ll) + (size lr)
+;;
+
+let rec height a =
+  match a with
+  | Nul -> -1
+  | Node (_,ll,lr) -> 1 + (max (height ll) (height lr))
+;;
+
+let rec nbOccu a b =
+  match (a,b) with
+  | (Nul,_) -> 0
+  | (Node (x,ll,lr),y) when x=y  -> 1 + (nbOccu ll b) + (nbOccu lr b)
+  | (Node (_,ll,lr),_) -> 0 + (nbOccu ll b) + (nbOccu lr b)
+;;
+
+let rec lengthDevelop a =
+  match a with
+  | Nul -> (-1)
+  | Node (_,ll,Nul) -> 1 + (size ll) + (lengthDevelop ll)
+  | Node (_,Nul,lr) -> 1 + (size lr) + (lengthDevelop lr)
+  | Node (_,ll,lr) -> 2 + (size ll) + (lengthDevelop ll) + (size lr) + (lengthDevelop lr)
+;;
+
+let rec prefix f a =
+  match a with
+  | Nul -> ()
+  | Node (v,ll,lr) ->
+    f v;
+    prefix f ll;
+    prefix f lr;
+    ()
+;;
+
+let rec infix f a =
+  match a with
+  | Nul -> ()
+  | Node (v,ll,lr) ->
+    infix f ll;
+    f v;
+    infix f lr;
+    ()
+;;
+
+let rec postfix f a =
+  match a with
+  | Nul -> ()
+  | Node (v,ll,lr) ->
+    postfix f ll;
+    f v;
+    postfix f lr;
+    ()
+;;
+
+(* ABR *)
+let rec isABR a =
+  match a with
+  | Nul -> true
+  | Node (v,Nul,Nul) -> true
+  | Node (v,ll,Nul) ->
+    if v<getRoot(ll) then
+      ((isABR ll))
+    else
+      false
+  | Node (v,Nul,lr) ->
+    if v<getRoot(lr) then
+      ((isABR lr))
+    else
+      false
+  | Node (v,ll,lr) ->
+    if v>getRoot(ll) then
+      begin
+        if v<getRoot(lr) then
+          ((isABR ll) && (isABR lr))
+        else
+          false
+      end
+    else
+     false
 ;;
--- example.ml
+++ example.ml
@@ -6,10 +6,22 @@
 
 open Bintree
 
-let a = (createTree 1 (createTree 2 (createTree 3 Nul Nul) (createTree 4 Nul Nul)) Nul);;
+let a = (createTree 5 (createTree 1 (createTree 0 Nul Nul) (createTree 4 Nul Nul)) (createTree 7 (createTree 6 Nul Nul) (createTree 9 Nul Nul)));;
+
+let applyFunc v =
+  printf "%u \n" v
+;;
 
 let _ =
   printf "%u \n" (nbLeaf a);
   printf "%u \n" (size a);
+  printf "%u \n" (height a);
+  printf "%u \n" (nbOccu a  1);
+  printf "%u \n" (lengthDevelop a);
+  prefix applyFunc a;
+  if (isABR a) then
+    printf "This binary tree is ABR"
+  else
+    printf "This binary tree is NOT ABR";
   ()
 ;;
